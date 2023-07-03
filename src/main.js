8f442c8f96cfa5d49b6ca8ace3b3b4ab9b00c306
Gavin Lee <GavinLee@ecs.corp>
2023-07-03 11:46:18 +0800

    update Readme

--- src/main.js
+++ src/main.js
@@ -4,11 +4,17 @@ import router from './router'
 import axios from 'axios'
 
 import vuetify from './plugins/vuetify'
+import RTSP2Web from 'rtsp2web'
 
 Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 Vue.config.performance = true
+
+const port = 8033
+new RTSP2web({
+    port
+})
 
 Vue.use(vuetify)
 new Vue({
